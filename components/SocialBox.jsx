86fafe4e4f554bca0aaf49eb16e8679049821315
Magnus Atli <magjrgylfason@gmail.com>
2023-01-16 09:54:54 +0000

    fixing socialbox size

--- components/SocialBox.jsx
+++ components/SocialBox.jsx
@@ -5,9 +5,9 @@ import { AiOutlineMail } from 'react-icons/ai'
 
 const SocialBox = () => {
     return (
-        <div id='socials' className='w-full lg:h-screen p-2'>
-            <div className='max-w-[1240px] mx-auto flex flex-col justify-center'>
-            <h2 className='py-4 text-blue-600 flex justify-center'>My Socials</h2>
+        <div id='socials' className='w-full py-8 h-screen'>
+            <div className='max-w-[1240px] mx-auto flex flex-col justify-center pt-8'>
+            <h2 className='py-8 text-blue-600 flex justify-center'>My Socials</h2>
                 <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
                     <Link href="https://www.linkedin.com/in/magn%C3%BAs-atli-gylfason-451555251/" target='_blank' rel='noreferrer'>
                         <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
